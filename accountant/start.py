de66b820a1f50a249eed396c3f16f03c521bdf88
imrantimkanov <itimkanov2014@litsey2.ru>
2020-09-08 20:24:51 +0300

    make it better

--- accountant/start.py
+++ accountant/start.py
@@ -7,8 +7,7 @@ from accountant.sqliteConnector import sqliteConnector
 import threading
 import time
 
-token = "1379702508:AAGquXO8II-Uzky60_YHzR7Ni6yddMQOZhg"
-bot = telebot.TeleBot(token=token)
+bot = telebot.TeleBot(token="1379702508:AAGquXO8II-Uzky60_YHzR7Ni6yddMQOZhg")
 moder = Mode()
 
 
